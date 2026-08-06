b821325dc13708877370522105372dcfc290e480
YukihiroArakawa <yukiburos@gmail.com>
2026-08-06 11:32:36 +0900

    feat(csharp): compare value and reference types

---
+++ 01-csharp-differences/TypeSystemSamples/PersonTypes.cs
@@ -0,0 +1,24 @@
+public class PersonClass
+{
+    public string Name { get; }
+
+    public PersonClass(string name) {
+        Name = name;
+    } 
+}
+
+public record PersonRecord(string Name);
+
+
+public struct PersonStruct
+{
+    public string Name;
+
+    public PersonStruct(string name) 
+    {
+        Name = name;
+    }
+}
+
+public record struct PersonRecordStruct(string Name);
+
--- 01-csharp-differences/TypeSystemSamples/Program.cs
+++ 01-csharp-differences/TypeSystemSamples/Program.cs
@@ -1,2 +1,28 @@
 ﻿string? name = null;
 Console.WriteLine(name?.Length ?? 0);
+
+var classFirst = new PersonClass("Ada");
+var classSecond = new PersonClass("Ada");
+
+var recordFirst = new PersonRecord("Ada");
+var recordSecond = new PersonRecord("Ada");
+
+Console.WriteLine(classFirst == classSecond);
+Console.WriteLine(recordFirst == recordSecond);
+Console.WriteLine(recordFirst.Equals(recordSecond));
+
+var structFirst = new PersonStruct("Ada");
+var structSecond = structFirst;
+structSecond.Name = "Grace";
+
+var recordStructFirst = new PersonRecordStruct("Ada");
+var recordStructSecond = recordStructFirst;
+recordStructSecond.Name = "Grace";
+
+Console.WriteLine(structFirst.Name);
+Console.WriteLine(structSecond.Name);
+Console.WriteLine(recordStructFirst.Name);
+Console.WriteLine(recordStructSecond.Name);
+
+Console.WriteLine(new PersonStruct("Ada").Equals(new PersonStruct("Ada")));
+Console.WriteLine(new PersonRecordStruct("Ada").Equals(new PersonRecordStruct("Ada")));
